3b1000fbf2617d2bb5415749c61fc10303ed2721
Anna Hakobyan <Anna.Hakobyan@helpsystems.com>
2021-08-06 11:51:26 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace testProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int a = 5;
+            int f = a;
+
+            Console.ReadLine();
+        }
+    }
+}
